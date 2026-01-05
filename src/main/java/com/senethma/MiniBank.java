d12d19d0f86688df29b19501e3d1c72cd4fc8ec8
Senethma Hewavitharana <senethmahewavitharana@gmail.com>
2026-01-05 12:45:11 +0530

    Creating the menu options and necessary classes

---
+++ src/main/java/com/senethma/MiniBank.java
@@ -0,0 +1,27 @@
+package com.senethma;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class MiniBank {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //variable to control the program execution which makes it easier to exit
+        boolean running = true;
+
+        System.out.println("Welcome to Mini Bank");
+
+        while (running) {
+            System.out.println("-------- Menu Options --------");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+
+            
+        }
+
+    }
+}
---
+++ src/main/java/com/senethma/MiniBankManager.java
@@ -0,0 +1,4 @@
+package com.senethma;
+
+public class MiniBankManager {
+}
