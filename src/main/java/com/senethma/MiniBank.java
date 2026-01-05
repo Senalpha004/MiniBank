c7cacb9378f9e983cc7c98354101464a1512bbdb
Senethma Hewavitharana <senethmahewavitharana@gmail.com>
2026-01-05 14:10:20 +0530

    Created the methods for each options

--- src/main/java/com/senethma/MiniBank.java
+++ src/main/java/com/senethma/MiniBank.java
@@ -1,12 +1,12 @@
 package com.senethma;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class MiniBank {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        MiniBankManager manager = new MiniBankManager(); //creating an object to access the manager class
 
         //variable to control the program execution which makes it easier to exit
         boolean running = true;
@@ -14,13 +14,41 @@ public class MiniBank {
         System.out.println("Welcome to Mini Bank");
 
         while (running) {
-            System.out.println("-------- Menu Options --------");
+            System.out.println("\n\n-------- Menu Options --------");
             System.out.println("1. Deposit");
             System.out.println("2. Withdraw");
             System.out.println("3. Check Balance");
             System.out.println("4. Exit");
 
-            
+            try {
+                int choice = input.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        manager.deposit();
+                        break;
+
+                        case 2:
+                            manager.withdraw();
+                            break;
+
+                            case 3:
+                                manager.checkBalance();
+                                break;
+
+                                case 4:
+                                    running = false;
+                                    break;
+
+                    default:
+                        System.out.println("Please choose a correct option!");
+                        break;
+                }
+
+            }catch (InputMismatchException e){
+                System.out.println("Invalid Input. Please try again");
+                input.next();
+            }
         }
 
     }
--- src/main/java/com/senethma/MiniBankManager.java
+++ src/main/java/com/senethma/MiniBankManager.java
@@ -1,4 +1,51 @@
 package com.senethma;
 
+import java.util.*;
+
 public class MiniBankManager {
+
+    Scanner sc = new Scanner(System.in);
+    static double balance = 0.0;
+
+    //method to deposit money
+    public void deposit(){
+        System.out.println("Enter amount to deposit: ");
+        double amount = sc.nextDouble();
+
+        if (amount <= 0){
+            System.out.println("Invalid amount! Amount must be more than 0.0 ");
+        }else {
+            balance += amount;
+            System.out.println("Deposited " + amount + " Successfully");
+        }
+    }
+
+    //method to withdraw money
+    public void withdraw(){
+
+        System.out.println("Enter amount to withdraw: ");
+        double amount = sc.nextDouble();
+
+        if (balance < amount){
+            System.out.println("Insufficient Balance!");
+        }else if (amount <= 0){
+            System.out.println("Insufficient Balance!");
+        }
+        else{
+            balance -= amount;
+            System.out.println("Withdrawn " + amount + " Successfully");
+        }
+
+    }
+
+    //method to check balance
+    public void checkBalance(){
+        if (balance <= 0){
+            System.out.println("No Balance left!");
+        }else {
+            System.out.println("Balance is " + balance);
+        }
+    }
+
 }
+
